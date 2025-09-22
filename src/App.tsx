4755f9c2869a6fe63e84fe88a566a23a7f4855d9
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-09-22 11:18:57 +0200

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
 import CardComp from "./components/CardComp"
+import cards from "./data/cards.json"
+import type { TCard } from "./types/card.types"
 
 const App = () => {
+	console.log(cards)
+	const [gameCards, setGameCards] = useState<TCardList[]>(cards)
+	console.log(gameCards)
 	return (
-		<div>
+		<div className="main_section">
 			<h1>Memory Game</h1>
-			<CardComp />
+			<div className="card_container">
+				{cards.map((card: TCard) => (
+					<CardComp key={card.id} />
+				))}
+			</div>
 		</div>
 	)
 }
